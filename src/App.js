a2a4fc68622a6495e6b9ecd001a156de75eef8c6
Kendall2106 <kenbrojim@gmail.com>
2023-07-23 07:17:31 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ProjectsSection from './Containers/Projects/projectsSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="text-bg">
+              <span>Cementerio de libros</span>
+              <h1>Olvidados</h1>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="text_img">
+            <img src="/img/frontPage.png" alt="Front Page" />
+          </div>
+        </div>
+      </div>
       </header>
-    </div>
+      <body>
+        <ProjectsSection />
+      </body>
+    </main>
   );
 }
 
---
+++ src/Containers/Projects/projectsSection.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import './projectsSection.css';
+
+const ProjectsSection = () => {
+  return (
+    <section>
+      <div class="three_box">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="box_text">
+                        <img className='imagen-proyects' src="/img/cuento.jpeg"
+                        alt='Primer fotografia'/>
+                        <br />
+                        <br />
+                        <button class="fill" id="btnAbrirModal">Cuento</button>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="box_text">
+                        <img className='imagen-proyects' src="/img/Relato.jpeg"
+                        alt='Primer fotografia'/>
+                        <br />
+                        <br />
+                        <button class="fill" id="btnAbrirModal">Relato</button>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="box_text">
+                        <img className='imagen-proyects' src="/img/Poesia.jpeg"
+                        alt='Primer fotografia'/>
+                        <br />
+                        <br />
+                        <button class="fill" id="btnAbrirModal">Poesia</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
