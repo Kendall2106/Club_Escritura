cc401e6c2da58b754125aea1b6e049f3dc07e910
Kendall2106 <kenbrojim@gmail.com>
2023-07-24 15:10:25 -0600

    text

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import ProjectsSection from './Containers/Projects/projectsSection';
 function App() {
   return (
     <main>
-      <header>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="text-bg">
-              <span>Cementerio de libros</span>
-              <h1>Olvidados</h1>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="text_img">
-            <img src="./img/frontPage.png" alt="Front Page" />
-          </div>
-        </div>
+     
+        <header>
+        <div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-6">
+      <div className='title'>
+         <span>Cementerio de libros</span><br />
+         <h1>Olvidados</h1>
       </div>
-      </header>
+    </div>
+    <div class="col-sm-6">
+      <div className="text_img">
+        <img src="./img/frontPage.png" alt="Front Page" />
+      </div>
+    </div>
+  </div>
+</div>
+
+        </ header>
+
         <ProjectsSection /> 
     </main>
   );
 }
 
 export default App;
+
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          
+          
+          
+        </header>
